f3ea5991bf65804b1082aff01fc94d665226bfd3
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-08 14:45:50 +0430

    Contract deployed

--- Blockchain_Data_Storage/backend/hardhat.config.ts
+++ Blockchain_Data_Storage/backend/hardhat.config.ts
@@ -1,8 +1,20 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config()
+
+const GOERLI_PRIVATE_KEY: string | undefined = process.env.GOERLI_PRIVATE_KEY;
+const ALCHEMY_API_KEY_URL: string | undefined = process.env.ALCHEMY_API_KEY_URL;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY]: []
+    }
+  }
 };
 
 export default config;
--- Blockchain_Data_Storage/backend/scripts/deploy.ts
+++ Blockchain_Data_Storage/backend/scripts/deploy.ts
@@ -1,18 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const storageFactory = await ethers.getContractFactory("imageStorage");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const deployedStorageContract = await storageFactory.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await deployedStorageContract.deployed();
 
-  await lock.deployed();
+  const contractAddress: string = deployedStorageContract.address;
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("Contract Address here", contractAddress)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
