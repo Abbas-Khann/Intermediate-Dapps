1e266573abde49937cc8b9c0ffd7349990153e76
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-12 12:24:15 +0430

    Set up config file

--- Crowd_Funding_Dapp/backend/hardhat.config.ts
+++ Crowd_Funding_Dapp/backend/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+const GOERLI_PRIVATE_KEY: string | undefined = process.env.GOERLI_PRIVATE_KEY;
+const RPC_URL: string | undefined = process.env.ALCHEMY_API_KEY_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: RPC_URL,
+      accounts: GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY]: []
+    }
+  }
 };
 
 export default config;
