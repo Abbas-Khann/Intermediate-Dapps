aa289cd0bcc28cb368ced3f4acbbd239590968be
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-13 22:46:53 +0430

    Added link to home page onClick

--- Crowd_Funding_Dapp/frontend/components/Navbar.tsx
+++ Crowd_Funding_Dapp/frontend/components/Navbar.tsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <div className="bg-gradient-to-r from-[#b7c3d3] to-[whitesmoke]">
         <nav className="flex items-center justify-evenly sm:justify-center px-8 bg-gradient-to-r from-[#212b3c] to-[#112B3C] shadow-2xl py-5">
-        <h1 className='text-xl sm:text-3xl text-[#FCF8E8]'>Crowd Funding</h1>
+        <Link href="/">
+        <h1 className='text-xl sm:text-3xl text-[#FCF8E8] cursor-pointer'>Crowd Funding</h1>
+        </Link>
         <div className='flex flex-auto justify-center items-center px-4 text-white text-sm sm:text-lg'>
         <Link href="/viewRequests">
         <button className='hover:border-b-2 border-blue-300'>View Requests</button>
