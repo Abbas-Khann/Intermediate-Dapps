2c427310dc107cfa9a5130f27212933dfad64337
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-13 21:47:56 +0430

    Created New page and component

---
+++ Crowd_Funding_Dapp/frontend/components/RequestCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RequestCard = () => {
+  return (
+    <div>RequestCard</div>
+  )
+}
+
+export default RequestCard
---
+++ Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import RequestCard from '../components/RequestCard'
+
+const viewRequests = () => {
+  return (
+    <div>
+        <Navbar />
+        <RequestCard />
+    </div>
+  )
+}
+
+export default viewRequests
