14a2bcd6698ed3f17c38365b2e368f841effce3f
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-14 14:05:44 +0430

    Fixed flex-wrap issue

--- Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
+++ Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import RequestCard from '../components/RequestCard'
 
 const viewRequests = () => {
   return (
-    <div>
+    <div className=''>
         <Navbar />
-        <div className='py-10 flex justify-around'>
+        <div className='py-10 flex justify-around flex-wrap gap-y-20 gap-x-14'>
+        <RequestCard />
         <RequestCard />
         <RequestCard />
         <RequestCard />
         </div>
+        <Footer />
     </div>
   )
 }
