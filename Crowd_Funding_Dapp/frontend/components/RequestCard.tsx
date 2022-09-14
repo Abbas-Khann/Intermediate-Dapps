15ea627db01af4e39bc79c9ba40d77b6bc89a70d
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-14 12:46:26 +0430

    Added image

--- Crowd_Funding_Dapp/frontend/components/RequestCard.tsx
+++ Crowd_Funding_Dapp/frontend/components/RequestCard.tsx
@@ -5,7 +5,8 @@ import CardImage from '../public/cardImg.png';
 const RequestCard = () => {
   return (
     <div className='bg-gradient-to-r from-[#212B3C] to-[#112B3C] rounded-2xl max-w-xs max-h-2xl px-7 text-white pb-7'>
-        <div className='flex items-center justify-end py-5'>
+        <div className='flex items-center justify-between py-5'>
+            <p className='text-lg'>Charity for Afghanistan</p>
             <button className='bg-[#212B3C] border-2 border-white rounded-2xl text-lg px-3 py-0.5'>Vote</button>
         </div>
         <div className='flex items-center'>
