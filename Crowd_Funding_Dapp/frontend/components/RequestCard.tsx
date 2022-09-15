74d23988e9c3b8755beebf2edfbfa8a19dd1fcd0
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-15 15:53:02 +0430

    Made the cards data dynamic

--- Crowd_Funding_Dapp/frontend/components/RequestCard.tsx
+++ Crowd_Funding_Dapp/frontend/components/RequestCard.tsx
@@ -4,7 +4,9 @@ import CardImage from '../public/cardImg.png';
 import { useProvider, useSigner, useContract } from 'wagmi';
 import { abi, CONTRACT_ADDRESS } from '../constants';
 
-const RequestCard = () => {
+const RequestCard = (props: any) => {
+
+    const {request, idx} = props
 
     const provider = useProvider();
     const { data: signer } = useSigner();
@@ -18,8 +20,12 @@ const RequestCard = () => {
   return (
     <div className='bg-gradient-to-r from-[#212B3C] to-[#112B3C] rounded-2xl max-w-xs max-h-2xl px-7 text-white pb-7'>
         <div className='flex items-center justify-between py-5'>
-            <p className='text-lg'>Charity for Afghanistan</p>
+            <p className='text-lg'>{request.description}</p>
             <button className='bg-[#212B3C] border-2 border-white rounded-2xl text-lg px-3 py-0.5'>Vote</button>
+        </div>
+        <div className='inline'>
+        <p>Recipient Address</p>
+        <p className='text-xs inline-block'>{request.recipient}</p>
         </div>
         <div className='flex items-center'>
             <Image src={CardImage} height={270} width={290} alt="Card-Image" />
--- Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
+++ Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import RequestCard from '../components/RequestCard'
+import { useProvider, useSigner, useContract } from 'wagmi';
+import { abi, CONTRACT_ADDRESS } from '../constants';
 
 const viewRequests = () => {
+
+  const [requestsArray, setRequestsArray] = useState<[]>([])
+  
+  const provider = useProvider();
+    const { data: signer } = useSigner();
+    const contract = useContract({
+        addressOrName: CONTRACT_ADDRESS,
+        contractInterface: abi,
+        signerOrProvider: signer || provider
+    });
+
+    const fetchReqs = async (_val: number): Promise <void> => {
+      try {
+        const req = await contract.request(_val);
+        await req;
+        return req;
+      } catch (err: any) {
+        console.log(err);
+        alert(err.reason);
+      }
+    }
+
+    const fetchRequests = async (): Promise <void> => {
+      try {
+        const amountOfRequests: number = await contract.numOfRequests();
+        console.log(amountOfRequests.toString())
+        const promises: [] = [];
+        for(let i: number = 0; i < amountOfRequests; i++) {
+          const promisedReq = await fetchReqs(i);
+          promises.push(promisedReq);
+        }
+        const requests = await Promise.all(promises);
+        console.log(requests);
+        setRequestsArray(requests);
+      } 
+      catch (err: any) {
+        console.error(err);
+        alert(err.reason);  
+      }
+    }
+
+    const renderRequests: JSX.Element[] = requestsArray.map((request, idx) => {
+      return <RequestCard key={idx} request={request} idx={idx} />
+    })
+    
+    useEffect(() => {
+      fetchRequests()
+    }, [])
+
   return (
     <div className=''>
         <Navbar />
@@ -12,10 +63,7 @@ const viewRequests = () => {
           <p className='border-b-2 '>Deadline: 3 Hours</p>
         </div>
         <div className='py-10 flex justify-around flex-wrap gap-y-20 gap-x-14'>
-        <RequestCard />
-        <RequestCard />
-        <RequestCard />
-        <RequestCard />
+        {renderRequests}
         </div>
         <Footer />
     </div>
