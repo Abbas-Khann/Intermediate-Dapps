d6bb54c6d9aa5d4ced6ad2bea2c859a8c0a75c15
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-15 10:45:17 +0430

    Cleaned up

--- Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
+++ Crowd_Funding_Dapp/frontend/pages/viewRequests.tsx
@@ -7,6 +7,10 @@ const viewRequests = () => {
   return (
     <div className=''>
         <Navbar />
+        <div className='text-[#112B3C] text-center text-2xl pt-10 pb-4'>
+          <p>Target: 3 Eth</p>
+          <p className='border-b-2 '>Deadline: 3 Hours</p>
+        </div>
         <div className='py-10 flex justify-around flex-wrap gap-y-20 gap-x-14'>
         <RequestCard />
         <RequestCard />
