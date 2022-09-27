4e9f29a84e0412da78df50059421cdb1b1c21073
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-27 19:32:19 +0430

    Added hero section

--- Calend3/frontend/pages/index.tsx
+++ Calend3/frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 
 const Home: NextPage = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   )
 }
