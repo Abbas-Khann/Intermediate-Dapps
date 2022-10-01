e48e3ed67636b2ce451582cbd867a7bcff6fc8e5
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-01 17:44:56 +0430

    Appointment completed function done

--- Calend3/frontend/components/AppointmentCard.tsx
+++ Calend3/frontend/components/AppointmentCard.tsx
@@ -71,6 +71,17 @@ const AppointmentCard = (props: any) => {
     }
   }
 
+  const cancelAppointment = async (id: number): Promise<void> => {
+    try {
+      const txn: any = await contract.cancelAppointment(id);
+      await txn.wait();
+    } 
+    catch (err: any) {
+       console.error(err)
+       alert(err.reason)  
+    }
+  }
+
   useEffect(() => {
     getAccurateStartingTime();
     getAccurateEndingTime();
@@ -112,7 +123,9 @@ const AppointmentCard = (props: any) => {
         </div>
       </div>
       <div className="flex justify-between pt-4">
-        <button className="px-3 py-1 border-2 rounded-md hover:bg-indigo-500 hover:transition-all hover:duration-500 hover:text-white">
+        <button className="px-3 py-1 border-2 rounded-md hover:bg-indigo-500 hover:transition-all hover:duration-500 hover:text-white"
+        onClick={() => cancelAppointment(idx)}
+        >
           Cancel
         </button>
         <button className="px-3 py-1 border-2 rounded-md hover:bg-indigo-500 hover:transition-all hover:duration-500 hover:text-white">
