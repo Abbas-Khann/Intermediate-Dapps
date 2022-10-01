5336f2b5cf97fc186d4254db7a17662071c2bc45
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-01 16:26:31 +0430

    Fetched mappings dynamically

--- Calend3/frontend/components/AppointmentCard.tsx
+++ Calend3/frontend/components/AppointmentCard.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const AppointmentCard = () => {
+const AppointmentCard = (props: any) => {
+
+  const { title, attendee, startingTime, endingTime, amountPaid } = props
+
   return (
     <div className="text-black w-10/12 sm:w-8/12 md:w-7/12 lg:w-5/12 px-10 py-4 bg-gradient-to-b from-violet-400 to-fuchsia-400 rounded-md mt-20">
       <div className="flex justify-between items-center pt-1 pb-4">
         <h3 className="border-l-4 border-black mb-2 p-1 md:text-xl text-white">
-          Crypto meeting
+          {title}
         </h3>
         <button className="px-3 py-1 border-2 rounded-md hover:bg-indigo-500 hover:transition-all hover:duration-500 hover:text-white">
           Withdraw
@@ -14,15 +17,15 @@ const AppointmentCard = () => {
       <div className="flex flex-col sm:px-12 px-2 py-5 bg-[#1C0238] text-white rounded-md ">
         <div className="flex justify-center mb-3">
           <h4 className="border-l-2 border-pink-400 mb-2 p-1">Address:</h4>
-          <h4 className=" mt-1 break-all sm:pl-8">
-            0x7B4A8d0862F049E35078E49F2561630Fac079eB9
+          <h4 className="mt-1 break-all sm:pl-8">
+            {attendee}
           </h4>
         </div>
         <div className="flex">
           <h4 className="border-l-2 border-pink-400 mb-2 p-1">
             Starting Time:
           </h4>
-          <h4 className="pl-2 mt-1">10:30 pm</h4>
+          <h4 className="pl-2 mt-1"> pm</h4>
         </div>
         <div className="flex">
           <h4 className="border-l-2 border-pink-400 mb-2 p-1">Ending Time:</h4>
--- Calend3/frontend/pages/ViewAppointments.tsx
+++ Calend3/frontend/pages/ViewAppointments.tsx
@@ -1,15 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppointmentCard from '../components/AppointmentCard'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { useContract, useProvider, useSigner } from 'wagmi'
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from '../constants'
 
-const ViewAppointments = () => {
+const ViewAppointments = (): JSX.Element => {
+
+  const [appointmentsArray, setAppointmentsArray] = useState<any[]>([]);
+
+
+  const provider = useProvider();
+  const {data: signer} = useSigner();
+  const contract = useContract({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: CONTRACT_ABI,
+    signerOrProvider: signer || provider
+  });
+
+  const fetchMapping = async (mapNum: number): Promise<number | undefined> => {
+    try { 
+      const _appointmentId: number = await contract.appointments(mapNum);
+      // console.log(_appointmentId)
+      return _appointmentId;
+    } 
+    catch (err: any) {
+      console.log(err);
+      alert(err.reason);
+    }
+  }
+
+  const fetchAllAppointments = async (): Promise<void> => {
+    try {
+      const _amountOfAppointments: number = await contract.appointmentId();
+      const mappingsArray: any[] = [];
+      for(let i: number = 0; i < _amountOfAppointments; i++) {
+        const mappings = await fetchMapping(i);
+        mappingsArray.push(mappings);
+      }
+      const appointments: any[] = await Promise.all(mappingsArray);
+      console.log(appointments)
+      setAppointmentsArray(appointments);
+    } 
+    catch (err: any) {
+      console.error(err);
+      alert(err.reason);
+    }
+  }
+
+  const getAppointmentCard = appointmentsArray.map((app, idx) => {
+    return (
+      <AppointmentCard key={idx} {...app} idx={idx} />
+    )
+  })
+
+  useEffect(() => {
+    fetchAllAppointments()
+  }, [])
+
   return (
     <div className='min-h-screen bg-[#1C0238]'>
         <Navbar />
         <div className='flex justify-around flex-wrap gap-y-10 gap-x-10'>
-        <AppointmentCard />
-        <AppointmentCard />
+        {getAppointmentCard}
         </div>
         {/* <Footer /> */}
     </div>
