85225fa1aefe7ed326ccafe6d543465b309d8d02
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-10-23 21:43:34 +0430

    Contract Updated

--- Salary Management Dapp/backend/contracts/SalaryManagement.sol
+++ Salary Management Dapp/backend/contracts/SalaryManagement.sol
@@ -13,6 +13,7 @@ Contract functionalities and requirements
 => The contract should have an addFunds function to add funds to the smart contract.
 => The contract should also have a withdraw function for the owner to be able to withdraw funds from the contract.
 => The contract should have seperate arrays of positions so we can render them seperately.
+=> The employees should get paid on a monthly basis and owner should get a warning if the time isn't completed yet.
 => The owner should also be able to remove a particular employee from the array.
 */
 
@@ -46,6 +47,13 @@ contract SalaryManagement {
         _;
     }
 
+    ///  index always passed in the functions to find the employee is the employeeId which is from the common mapping we have created 
+    ///  the intern, junior or senior address array is just used when we want all the interns collectively 
+    /// otherwise we are not using this array anywhere to check that particular exsists or not
+    /// that will be checked from the Employee struct from the employee position
+
+    /// employee is the main mapping which consists of all the info for employee , we always use this to extract the information
+
     /* <=============== ADD EMPLOYEE ===============> */
     function addEmployee(
         string memory _name,
@@ -53,6 +61,7 @@ contract SalaryManagement {
         address _employeeAddress,
         Position _employeePosition
     ) public onlyOwner {
+        require(!isEmployee[_employeeAddress], "Already an employee");
         isEmployee[_employeeAddress] = true;
         if (_employeePosition == Position.Intern) {
             interns.push(_employeeAddress);
@@ -69,7 +78,7 @@ contract SalaryManagement {
             _employeeAddress,
             _employeePosition
         );
-        employeeIds[msg.sender] = employeeId;
+        employeeIds[_employeeAddress] = employeeId;
         employeeId++;
     }
 
@@ -89,20 +98,24 @@ contract SalaryManagement {
     }
 
     /* <=============== PAY A PARTICULAR INTERN ===============> */
+    /// index is the employeeId
     function payAParticularIntern(uint _index) public payable onlyOwner {
-        uint internMonthlyWage = 0.03 ether;
-        internMonthlyWage = msg.value;
-        require(_index < getInternsArrayLength(), "This index doesn't exist");
-        interns[_index] = interns[getInternsArrayLength() - 1];
-        (bool sent,) = interns[_index].call{value: internMonthlyWage}("");
+        uint internMonthlyWage = 0.01 ether;
+        // internMonthlyWage = msg.value;
+        // require(_index < getInternsArrayLength(), "This index doesn't exist");
+        Employee storage _employee = employee[_index] ;
+        require(_employee.employeePosition == Position.Intern ,"Not an Intern");
+        (bool sent,) = _employee.employeeAddress.call{value: internMonthlyWage}("");
         require(sent, "Failed to pay this intern");
     }
 
     /* <=============== PAY ALL INTERNS ===============> */
     function payAllInterns() public payable onlyOwner {
         uint internMonthlyWage = 0.03 ether;
+        // internMonthlyWage = msg.value;
         require(interns.length > 0, "There is no intern to pay");
         for (uint i = 0; i < interns.length; i++) {
+            require(interns[i] != address(0),"Not a valid address");
             (bool sent,) = interns[i].call{value: internMonthlyWage}("");
             require(sent, "Failed to send salary to interns");
         }
@@ -111,9 +124,8 @@ contract SalaryManagement {
     
     /* <=============== DELETE A PARTICULAR INTERN ===============> */
     function deleteIntern(uint _index) public onlyOwner {
-        require(_index < interns.length, "This index does not exist");
-        interns[_index] = interns[getInternsArrayLength() - 1];
-        interns.pop();
+        /// delete will delete the record of the employee from our main employee array
+        delete employee[_index];
     }
 
     /* <=============== GET JUNIOR ARRAY LENGTH ===============> */
@@ -126,6 +138,7 @@ contract SalaryManagement {
         uint juniorsMonthlyWage = 0.02 ether;
         require(juniors.length > 0, "There is no junior to pay");
         for (uint256 i = 0; i < juniors.length; i++) {
+            require(juniors[i] != address(0),"Not a valid address");
             (bool sent, ) = juniors[i].call{value: juniorsMonthlyWage }("");
             require(sent, "Failed to pay Juniors");
         }
@@ -133,18 +146,16 @@ contract SalaryManagement {
     
     /* <=============== PAY A PARTICULAR JUNIOR ===============> */
     function payAParticularJunior(uint _index) public payable onlyOwner {
-        require(_index < juniors.length, "This index doesn't exist");
         uint juniorsMonthlyWage = 0.02 ether;
-        juniors[_index] = juniors[getJuniorsArrayLength() - 1];
-        (bool sent,) = juniors[_index].call{value: juniorsMonthlyWage}("");
+        Employee storage _employee = employee[_index] ;
+        require(_employee.employeePosition == Position.Junior ,"Not a junior employee");
+        (bool sent,) = _employee.employeeAddress.call{value: juniorsMonthlyWage}("");
         require(sent, "Failed to pay this employee!");
     }    
 
     /* <=============== DELETE A PARTICULAR JUNIOR ===============> */
     function deleteJunior(uint _index) public onlyOwner {
-        require(_index < getJuniorsArrayLength(), "This index does not exist");
-        juniors[_index] = juniors[getJuniorsArrayLength() - 1];
-        juniors.pop();
+        delete employee[_index];
     }
     /* <=============== GET SENIOR ARRAY LENGTH ===============> */
 
@@ -158,6 +169,7 @@ contract SalaryManagement {
         uint seniorsMonthlyWage = 0.01 ether;
         require(seniors.length > 0, "There is no senior to pay");
         for (uint i = 0; i < seniors.length; i++) {
+            require(seniors[i] != address(0),"Not a valid address");
             (bool sent, ) = seniors[i].call{value: seniorsMonthlyWage}("");
             require(sent, "Failed to pay seniors!");
         }
@@ -165,18 +177,16 @@ contract SalaryManagement {
 
     /* <=============== PAY A PARTICULAR SENIOR ===============> */
     function payAParticularSenior(uint _index) public payable onlyOwner {
-        require(_index < seniors.length, "This index doesn't exist");
-        uint seniorsMonthlyWage = 0.01 ether;
-        seniors[_index] = seniors[getSeniorsArrayLength() - 1];
-        (bool sent,) = seniors[_index].call{value: seniorsMonthlyWage}("");
+        uint seniorsMonthlyWage = 0.03 ether;
+        Employee storage _employee = employee[_index] ;
+        require(_employee.employeePosition == Position.Senior ,"Not a Senior employee");
+        (bool sent,) = _employee.employeeAddress.call{value: seniorsMonthlyWage}("");
         require(sent, "Failed to pay this senior Employee");
     }
 
     /* <=============== DELETE A SENIOR ===============> */
     function deleteSenior(uint _index) public {
-        require(_index < getSeniorsArrayLength(), "This index does not exist");
-        seniors[_index] = seniors[getSeniorsArrayLength() - 1];
-        seniors.pop();
+        delete employee[_index];
     }
 
     /* <=============== ADD MONEY TO THE SMART CONTRACT ===============> */
@@ -191,6 +201,11 @@ contract SalaryManagement {
     function getContractBalance() public view onlyOwner returns (uint256) {
         return address(this).balance;
     }
+
+    // get the employee ID from the address and accordingly call other functions 
+    function getEmployeeId(address user) public view returns(uint256) {
+        return employeeIds[user]; 
+    } 
 
     /* <=============== WITHDRAW MONEY FROM CONTRACT ===============> */
     function withdraw() public onlyOwner {
