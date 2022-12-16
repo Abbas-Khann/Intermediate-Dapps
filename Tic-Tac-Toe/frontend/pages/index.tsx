19323c0e9a6f8e85d85e4014ca650fed224ed5d7
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-16 16:44:15 +0800

    Updated Game component

--- Tic-Tac-Toe/frontend/pages/index.tsx
+++ Tic-Tac-Toe/frontend/pages/index.tsx
@@ -1,13 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import logo from "../public/logo.png";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(): JSX.Element {
   return (
-      <div>
-          
+      <div className='bg-[#213745]'>
+        {/* Logo and header here */}
+        <div className='flex justify-center py-10'>
+          <Image src={ logo } alt={ '' } width={370} />
+        </div>
+        <div>
+          <h1 className=''>Bet Amount</h1>
+        </div>
+  
       </div>
   )
 }
