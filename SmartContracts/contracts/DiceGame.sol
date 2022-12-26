2fc14381ed234669af484788c1e2f4bd7d5f4704
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-26 12:29:16 +0800

    Added Dice roll functions

--- SmartContracts/contracts/DiceGame.sol
+++ SmartContracts/contracts/DiceGame.sol
@@ -11,19 +11,31 @@ pragma solidity ^0.8.4;
 */
 
 contract DiceGame {
+
     error NOT_ENOUGH_ETH();
     error ALREADY_JOINED();
     error NOT_OWNER();
-
+    error ONLY_PLAYERS_CAN_ROLL_DICE();
 
     address public immutable owner;
     uint256 public maxPlayers;
     address[] public players;
     uint256 public gameTime;
+    uint256 gameId;
+    uint256[] public playerOneNumbers;
+    uint256[] public playerTwoNumbers;
+    bool public playerOneMove;
+    bool public playerTwoMove;
+    uint256 playerOnePoints;
+    uint256 playerTwoPoints;
+    uint256 winningPoints;
 
     mapping(address => bool) public hasJoined;
-    constructor() payable {
+    mapping(address => uint256) public playerMoves;
+
+    constructor(uint256 _winningPoints) payable {
         owner = msg.sender;
+        winningPoints = _winningPoints;
     }
 
     modifier alreadyJoined() {
@@ -47,13 +59,95 @@ contract DiceGame {
         }
         if (players.length == 0) {
             startGameTime();
+            rollDicePlayerOne();
         }
         hasJoined[msg.sender] = true;
         players.push(msg.sender);
     }
 
+    modifier onlyPlayers() {
+        if(msg.sender != players[0] || msg.sender != players[1]) {
+            revert ONLY_PLAYERS_CAN_ROLL_DICE();
+        }
+        _;
+    }
+
+    modifier onlyPlayerOne {
+        if (msg.sender != players[0]) {
+            revert("NOT_PLAYER_ONE");
+        }
+        _;
+    }
+    modifier onlyPlayerTwo {
+        if (msg.sender != players[1]) {
+            revert("NOT_PLAYER_TWO");
+        }
+        _;
+    }
+
+    function rollDicePlayerOne() public onlyPlayerOne gameOngoing returns(uint256) {
+        require(playerOnePoints < winningPoints && playerTwoPoints < winningPoints,         "POINTS_EXCEEDED!!!");
+        require(playerTwoMove, "Wait for player Two to finish his move");
+        uint256 randomNumber = generateRandomNumber();
+        playerOneNumbers.push(randomNumber);
+        playerOnePoints = addPlayerOnePoints();
+        playerMoves[msg.sender] += 1;
+        playerOneMove = true;
+        playerTwoMove = false;
+        return playerOnePoints;
+    }
+
+    function rollDicePlayerTwo() public onlyPlayerTwo gameOngoing returns(uint256) {
+        require(playerOnePoints < winningPoints && playerTwoPoints < winningPoints,         "POINTS_EXCEEDED!!!");
+        require(playerOneMove, "Wait for player One to finish his move");
+        uint256 randomNumber = generateRandomNumber();
+        playerTwoNumbers.push(randomNumber);
+        playerTwoPoints = addPlayerTwoPoints();
+        playerMoves[msg.sender] += 1;
+        playerTwoMove = true;
+        playerOneMove = false;
+        return playerTwoPoints;
+    }
+
+    function checkWinner() external {
+        if(playerOnePoints >= winningPoints) {
+            
+            // playerOne wins
+        }
+        else if(playerTwoPoints >= winningPoints) {
+            
+            // playerTwo wins
+        }
+    }
+
+    // add prev points and winner maloomawal
+
+    function addPlayerOnePoints() private view returns(uint256) {
+        uint256 playerOneScores;
+        for(uint256 i = 0; i < playerOneNumbers.length; i++) {
+            playerOneScores += playerOneNumbers[i];
+        }
+        return playerOneScores;
+    }
+
+    function addPlayerTwoPoints() private view returns(uint256) {
+        uint256 playerTwoScores;
+        for(uint256 i = 0; i < playerTwoNumbers.length; i++) {
+            playerTwoScores += playerTwoNumbers[i];
+        }
+        return playerTwoScores;
+    }
+
+
+
+    function generateRandomNumber() internal view returns(uint256) {
+        uint256 rand = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 6 + 1;
+        return rand;
+    }
+
     function startGameTime() internal {
         gameTime = block.timestamp + 20 minutes;
+        gameId++;
     }
 
     modifier onlyOwner() {
@@ -71,6 +165,10 @@ contract DiceGame {
     returns (uint256) 
     {
         return address(this).balance;
+    }
+
+    function gameID() public view returns(uint256) {
+        return gameId;
     }
 
     
