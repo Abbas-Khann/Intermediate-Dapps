6788c0dae1d51af38f801bbbc18310d3a471314d
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-03-13 00:04:02 +0800

    Added tailwindcss & navbar

--- Poap/poap/pages/index.tsx
+++ Poap/poap/pages/index.tsx
@@ -1,11 +1,9 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Header from "../components/Header";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <main>
+    <main className="">
       <Header />
     </main>
   );
